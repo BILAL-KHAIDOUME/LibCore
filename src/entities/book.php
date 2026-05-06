30193ed6f66275a28978aa9e04216ea763863101
amine sakhri <sakhriaminee2003@gmail.com>
2026-05-06 12:45:33 +0200

    function getters et setters

--- src/entities/book.php
+++ src/entities/book.php
@@ -11,7 +11,23 @@ class Livre{
     $this -> auteur = $auteur ;
     $this -> type = $type ;
 
- }}
+ }
+
+ 
+ public function getNom() {return $this -> nom ;}
+ public function getAuteur() {return $this -> auteur ;}
+ public function getType() {return $this -> type ;}
+
+
+ public function setNom($nom) {$this -> nom =$nom ;}
+ public function setAuteur($auteur) {$this -> auteur =$auteur ;}
+ public function setType($type) {$this -> type =$type ;}
+
+}
+
+
+
+
 ?>
 
 
