e59c81b06e74cf0751b15a82b1a9f98c0cb9f7f9
amine sakhri <sakhriaminee2003@gmail.com>
2026-05-06 12:39:29 +0200

    creation class avec constructeur

--- src/entities/book.php
+++ src/entities/book.php
@@ -1,1 +1,20 @@
 
+<?php
+class Livre{
+    private $nom ;
+    private $auteur ;
+    private $type ;
+
+ public function __construct($nom  ,$auteur ,$type) 
+ {
+    $this -> nom = $nom ;
+    $this -> auteur = $auteur ;
+    $this -> type = $type ;
+
+ }}
+?>
+
+
+
+
+
