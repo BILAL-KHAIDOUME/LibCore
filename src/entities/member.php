040d48df1996a3a908252a4d7a94a62b2c2071a1
amine sakhri <sakhriaminee2003@gmail.com>
2026-05-08 12:51:50 +0200

    /

--- src/entities/member.php
+++ src/entities/member.php
@@ -1,13 +1,45 @@
+<?php
 
+require_once __DIR__ . "/../entities/Connexion.php";
+require_once __DIR__ . "/../entities/Member.php";
 
+class MemberRepository {
 
+    private $conn;
 
+    public function __construct() {
 
+        $database = new Database();
+        $this->conn = $database->connect();
 
+    }
 
+    // Ajouter membre
+    public function add($member) {
 
+        $sql = "INSERT INTO members(nom, email, type)
+                VALUES (?, ?, ?)";
 
+        $stmt = $this->conn->prepare($sql);
 
+        return $stmt->execute([
 
+            $member->getNom(),
+            $member->getEmail(),
+            $member->getType()
 
+        ]);
 
+    }
+
+    public function getAll() {
+
+        $sql = "SELECT * FROM members";
+
+        $stmt = $this->conn->query($sql);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    }
+
+}
