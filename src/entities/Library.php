4fe480a097ee64629e231f1bc6a7f64159500bec
amine sakhri <sakhriamine2003@gmail.com>
2026-05-09 23:56:21 +0200

    AjoutrMember en data

--- src/entities/Library.php
+++ src/entities/Library.php
@@ -26,4 +26,32 @@ class Library {
             $book->getIsbn(),
             $book->getDisponible()
         ]);
+    }
+
+
+
+    
+        public function AjoutrMember(Member $member) {
+
+        $sqlUser = "INSERT INTO users(nom, prenom, dateC) VALUES (?, ?, ?)";
+
+        $stmtUser = $this->conn->prepare($sqlUser);
+
+        $stmtUser->execute([
+            $member->getNom(),
+            $member->getPrenom(),
+            $member->getDateC()
+        ]);
+
+        $user_id = $this->conn->lastInsertId();
+
+        $sqlMember = "INSERT INTO membres(role_id, user_id)
+                      VALUES (?, ?)";
+
+        $stmtMember = $this->conn->prepare($sqlMember);
+
+        return $stmtMember->execute([
+            $member->getRoleId(),
+            $user_id
+        ]);
     }}
